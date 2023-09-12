cf674842aa4f6632fd606eba951c66ad82d8597d
Joydip <joydip.mondal@gmail.com>
2023-09-12 13:59:55 +0530

    assigment-1 uploaded

--- codes/day-1/HelloWorldApp/Program.cs
+++ codes/day-1/HelloWorldApp/Program.cs
@@ -10,14 +10,26 @@ namespace HelloWorldExecuatbleApp
         //static void Main(string[] args)
         static void Main()
         {
+            //value-type or primitive type data type - int
+            //value => value-type variable/identifier
+            int value = 100;
+            Console.WriteLine(value);
+
+            CallMessenger();
+            Console.WriteLine("press any key to terminate...");
+            Console.ReadKey();
+        }
+        static void CallMessenger()
+        {
             //fully qualified name
             //Welcome.Messenger messenger = new Welcome.Messenger();
 
-            Messenger messenger = new Messenger();
+            //reference-type data type - Messenger class
+            //messenger =>reference-type variable/identifier
+            Messenger messenger = null;
+            messenger = new Messenger();
             string result = messenger.SayHello("joydip");
             Console.WriteLine(result);
-            Console.WriteLine("press any key to terminate...");
-            Console.ReadKey();
         }
     }
 }
