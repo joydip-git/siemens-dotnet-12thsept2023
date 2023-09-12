60d71d3782e655316951b800976e9664adcc908f
Joydip <joydip.mondal@gmail.com>
2023-09-12 17:21:37 +0530

    day-1 final upload with two-dim array and assignment

---
+++ codes/day-1/ArrayDemo/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace ArrayDemo
+{
+    internal class Program
+    {
+        static void SimpleArray()
+        {
+            //Array numbers = new Array();
+            int[] numbers = new int[3];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write("enter value at index-" + i + ": ");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("\n");
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.WriteLine("value at numbers[" + i + "]: " + numbers[i]);
+            //}
+            foreach (int item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void TwoDimensionalDemo()
+        {
+            int[,] ints = new int[3, 3];
+        }
+        static void Main()
+        {
+            int length = GetLength();
+            int[] numbers = new int[length];
+
+            EnterValuesInTheArray(numbers);
+            PrintMenu();
+            int choice = GetChoice();
+            SortArray(numbers, choice);
+            PrintArrayValues(numbers);
+        }
+        static int GetLength()
+        {
+            Console.Write("enter size of the array: ");
+            return int.Parse(Console.ReadLine());
+        }
+        static void EnterValuesInTheArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = GetValue(i);
+            }
+        }
+        static int GetValue(int index)
+        {
+            Console.Write("enter value for index-" + index + ": ");
+            return int.Parse(Console.ReadLine());
+        }
+        static void PrintMenu()
+        {
+            Console.WriteLine("1. sort array in ascending order");
+            Console.WriteLine("2. sort array in descending order");
+        }
+        static int GetChoice()
+        {
+            Console.Write("\nenter choice[1/2]: ");
+            return int.Parse(Console.ReadLine());
+        }
+        static void SortArray(int[] arr, int sortChoice)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {                    
+                    switch (sortChoice)
+                    {
+                        case 1:
+                            if( arr[i] > arr[j])
+                            {
+                                int temp = arr[i];
+                                arr[i] = arr[j];
+                                arr[j] = temp;
+                            }
+                            break;
+
+                        case 2:
+                            if (arr[j] > arr[i])
+                            {
+                                int temp = arr[j];
+                                arr[j] = arr[i];
+                                arr[i] = temp;
+                            }
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+            }
+
+        }        
+        static void PrintArrayValues(int[] numbers)
+        {
+            Console.WriteLine("\nafter sorting\n");
+            foreach (int item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ codes/day-1/CalculatorApp/CalculationUserInterface/Utilities/UiUtility.cs
@@ -0,0 +1,64 @@
+﻿using CalculationLibrary;
+//using static System.Console;
+using System;
+
+namespace CalculationUserInterface.Utilities
+{
+    internal static class UiUtility
+    {
+        public static void PrintMenu()
+        {
+            Console.WriteLine("1. Add(+)");
+            Console.WriteLine("2. Subtract(-)");
+            Console.WriteLine("3. Multiply(*)");
+            Console.WriteLine("4. Divide(/)");
+            Console.WriteLine("5. Exit(e)");
+        }
+        public static char GetChoice()
+        {
+            Console.Write("\nEnter a choice[+,-,*,/,e]: ");
+            return char.Parse(Console.ReadLine());
+        }
+        public static int GetNumber()
+        {
+            Console.Write("\nEnter a number: ");
+            return int.Parse(Console.ReadLine());
+        }
+        public static int Calculate(char choice, int first, int second)
+        {
+            int result = 0;
+            Calculation calculation = new Calculation();
+
+            switch (choice)
+            {
+                case '+':
+                    result = calculation.Add(first, second);
+                    break;
+                case '-':
+                    result = calculation.Subtract(first, second);
+                    break;
+                case '*':
+                    result = calculation.Multiply(first, second);
+                    break;
+                case '/':
+                    result = calculation.Divide(first, second);
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+        public static char DecideToContinue()
+        {
+            Console.Write("\nWould you like to continue?[y/Y/n/N]: ");
+            char ch = char.Parse(Console.ReadLine());
+            return ConvertCharacterToLowerCase(ch);
+        }
+        private static char ConvertCharacterToLowerCase(char ch)
+        {
+            if (char.IsUpper(ch))
+                ch = char.ToLower(ch);
+            return ch;
+        }
+    }
+}
