a7b31db39278eebdfa8afda5932b0e0d6742f746
Joydip <joydip.mondal@gmail.com>
2023-09-12 12:30:13 +0530

    day-1 pre-lunch update

---
+++ codes/day-1/HelloWorldApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;//mscorlib.dll
+using Welcome; //WelcomeLibrary.dll
+
+namespace HelloWorldExecuatbleApp
+{
+    internal class Program
+    {
+        //static int Main()
+        //static int Main(string[] args)
+        //static void Main(string[] args)
+        static void Main()
+        {
+            //fully qualified name
+            //Welcome.Messenger messenger = new Welcome.Messenger();
+
+            Messenger messenger = new Messenger();
+            string result = messenger.SayHello("joydip");
+            Console.WriteLine(result);
+            Console.WriteLine("press any key to terminate...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ codes/day-1/WelcomeLibrary/Messenger.cs
@@ -0,0 +1,10 @@
+﻿namespace Welcome
+{
+    public class Messenger
+    {
+        public string SayHello(string name)
+        {
+            return "Welcome to C# " + name;
+        }
+    }
+}
