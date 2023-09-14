5ec52a3190a1c30c7b0a428f454d2c6ed8b7f133
Joydip <joydip.mondal@gmail.com>
2023-09-14 10:46:48 +0530

    Equality discussion

---
+++ codes/day-3/OperatorOverloadingDemo/Person.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace OperatorOverloadingDemo
+{
+    internal class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Person() { }
+
+        public Person(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public override int GetHashCode()
+        {
+            const int prime = 31;
+            int hash = this.Id.GetHashCode() * prime + prime;
+            hash = this.Name.GetHashCode() * hash + prime;
+            return hash;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+
+            if (obj is Person)
+            {
+                Person p = (Person)obj;
+
+                if (p.Id != this.Id)
+                    return false;
+
+                if (!p.Name.Equals(this.Name))
+                    return false;
+
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public static bool operator ==(Person first, Person second)
+        {
+            if (ReferenceEquals(first, second))
+                return true;
+            else
+                return first.Equals(second);
+
+        }
+        public static bool operator !=(Person first, Person second)
+        {
+            if (ReferenceEquals(first, second))
+                return false;
+            else
+                return !first.Equals(second);
+        }
+        public static bool operator >(Person first, Person second)
+        {
+            if (first.Id > second.Id)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator <(Person first, Person second)
+        {
+            if (first.Id < second.Id)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ codes/day-3/OperatorOverloadingDemo/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OperatorOverloadingDemo
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string first = "Siemens";
+            string second = "Bangalore";
+            string location = first + ", " + second;
+            Console.WriteLine(location);
+
+            Person anilPerson = new Person(1, "anil");
+            Person sunilPerson = new Person(1, "anil");
+            //if(anilPerson.Equals(sunilPerson){}
+
+            //first-operand operator second-operand
+            //if (anilPerson == sunilPerson)
+            //{
+            //    Console.WriteLine("same");
+            //}
+            //else
+            //    Console.WriteLine("different");
+            int anilPersonHash = anilPerson.GetHashCode();
+            int sunilPersonHash = sunilPerson.GetHashCode();
+            if (anilPersonHash == sunilPersonHash)
+            {
+                //if (anilPerson.Equals(sunilPerson)
+                if (anilPerson == sunilPerson)
+                    Console.WriteLine("same");
+            }
+            else
+                Console.WriteLine("different");
+
+            HashSet<int> set = new HashSet<int>();
+            set.Add(1);
+            set.Add(2);
+            set.Add(1);
+
+            foreach (int item in set)
+            {
+                Console.WriteLine(item);
+            }
+
+            HashSet<Person> people = new HashSet<Person>();
+            people.Add(anilPerson);
+            people.Add(sunilPerson);
+            foreach (Person person in people)
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
